b2342b42f0c5cd20f06ccec370b653174eadd675
Freekeyboy <jjhutton81@gmail.com>
2019-10-07 20:47:43 -0700

    Skills page styled for desktop.

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -10,7 +10,20 @@ export default class Skills extends Component {
   render() {
     return (
       <div className='page-wrap'>
-          <img className='skillsFX' src={SkillFX} alt='Logo' />
+        <img className='skillsFX' src={SkillFX} alt='Logo' />
+        <div className='skillText'>
+          <p>
+            I was contemplating how I would list off my skillset. I could just
+            list them off, but that would be boring. I could make a chart, but
+            that’s so common it might as well be boring. So why not make it a
+            little game? That’s way more fun.{' '}
+          </p>
+          <h2>
+            To play simply click and hold on each card, then quickly drag to the
+            left or right and release. Swipe left or right if you’re on a
+            touchscreen device.{' '}
+          </h2>
+        </div>
         <div className='skills-deck-wrap'>
           <SkillsDeck />
         </div>
