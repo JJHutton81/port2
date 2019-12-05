b8984cab245efd961be04012fc91adc7a618a20a
Freekeyboy <jjhutton81@gmail.com>
2019-12-04 16:31:42 -0800

    Styled aboutme page for mobile.

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import './about_me.css';
 
@@ -10,19 +11,15 @@ import Needle from '../../media/needle.png';
 export default class AboutMe extends Component {
   render() {
     return (
-      <div className='aboutMeBody'>
-        <img className='headImg' src={HeadGrapix} alt='Logo' />
-        <div className='spaceNeedleHolder'>
-          <img className='aboutFX' src={About} alt='Logo' />
-          <img className='needle' src={Needle} alt='Space Needle' />
-          <div className='aboutHeadText'>
-            <img
-              // className='head'
-              src={Headshot}
-              alt='Headshot photo of James Hutton'
-            />
-            <section>
-              <p>
+      <Body>
+        <HeadGraphic src={HeadGrapix} alt='Logo' />
+        <GraphicWrap>
+          <AboutFX src={About} alt='Logo' />
+          <NeedleFX src={Needle} alt='Space Needle' />
+          <TextWrap>
+            <Head src={Headshot} alt='Headshot photo of James Hutton' />
+            <Section>
+              <P>
                 Hello again, and thank you for visiting my website. I started my
                 journey as a classically trained artist. As I grew older, I
                 found myself getting pulled more and more to the more
@@ -34,11 +31,91 @@ export default class AboutMe extends Component {
                 of all creative apps. I love clever animations and tricks that
                 amplify the user experience. If you like what you see, hit me
                 up. Let’s make something special together.{' '}
-              </p>
-            </section>
-          </div>
-        </div>
-      </div>
+              </P>
+            </Section>
+          </TextWrap>
+        </GraphicWrap>
+      </Body>
     );
   }
 }
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  // @media (max-width: 768px) {
+  //   height: auto;
+  // }
+`;
+
+const HeadGraphic = styled.img`
+  width: 100%;
+`;
+
+const AboutFX = styled.img`
+  width: 30%;
+  float: left;
+  margin: 6% 0 3% 6%;
+  @media (max-width: 768px) {
+    width: 90%;
+    margin: 2% auto;
+  }
+`;
+
+const GraphicWrap = styled.div`
+  background-size: contain;
+  height: 500px;
+  width: 99.8%;
+  position: absolute;
+  margin-top: 7%;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const TextWrap = styled.div`
+  display: flex;
+  width: 80%;
+  margin: 16% auto;
+  background-color: white;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    margin: 1% auto;
+  }
+`;
+
+const Head = styled.img`
+  height: 280px;
+  @media (max-width: 768px) {
+    width: 40%;
+    border-radius: 50%;
+  }
+`;
+
+const Section = styled.section`
+  z-index: 1;
+  background: white;
+`;
+
+const P = styled.p`
+  font-size: 1.6rem;
+  padding: 2.5%;
+  text-align: justify;
+  z-index: 1;
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+const NeedleFX = styled.img`
+  width: 15%;
+  height: 500px;
+  margin-left: 6.5%;
+  position: absolute;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
